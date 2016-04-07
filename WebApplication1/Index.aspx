8f71b3168d2ba2df83884825110dafb27ec2ef54
onilawasura <haonila@gmail.com>
2016-04-07 14:52:53 +0530

    Initial commit

---
+++ WebApplication1/Index.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="WebApplication1.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <tr>
+                <td>
+                    <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+                </td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/Index.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["HMSConnectString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand com = new SqlCommand("SELECT * FROM Tbl_DoctorLogin", con);
+                con.Open();
+                GridView1.DataSource = com.ExecuteReader();
+                GridView1.DataBind();
+            }
+        }
+    }
+}
